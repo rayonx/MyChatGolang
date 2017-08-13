716709fa8849adde70feb79019852b0e41ff5006
Ray <ray@oceannexus.my>
2017-08-14 02:07:52 +0800

    Changed the handlers to use goroutines

--- lib/main.go
+++ lib/main.go
@@ -6,13 +6,18 @@ import (
 	"log"
 	"net"
 	"os"
+	"sync"
 )
 
 const port = "8080"
 
-// RunHost takes an ip as argument
-// and listens for connections on that ip
+// RunHost takes an IP address as argument
+// and listens for connection on that IP
 func RunHost(ip string) {
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+
 	ipAndPort := ip + ":" + port
 	listener, listenErr := net.Listen("tcp", ipAndPort)
 	if listenErr != nil {
@@ -24,57 +29,78 @@ func RunHost(ip string) {
 	if acceptErr != nil {
 		log.Fatal("Error: ", acceptErr)
 	}
-	fmt.Println("New connection accepted")
+	fmt.Println("New Connection Accepted")
 
+	go handleHostRead(conn, &wg)
+	go handleHostSend(conn, &wg)
+
+	wg.Wait()
+
+}
+
+func handleHostRead(conn net.Conn, wg *sync.WaitGroup) {
 	for {
-		handleHost(conn)
+		reader := bufio.NewReader(conn)
+		message, readErr := reader.ReadString('\n')
+		if readErr != nil {
+			log.Fatal("Error", readErr)
+			wg.Done()
+		}
+		fmt.Println("Message received: ", message)
 	}
 
 }
 
-func handleHost(conn net.Conn) {
-	reader := bufio.NewReader(conn)
-	message, readErr := reader.ReadString('\n')
-	if readErr != nil {
-		log.Fatal("Error: ", readErr)
+func handleHostSend(conn net.Conn, wg *sync.WaitGroup) {
+	for {
+		replyReader := bufio.NewReader(os.Stdin)
+		replyMessage, replyErr := replyReader.ReadString('\n')
+		if replyErr != nil {
+			log.Fatal("Error", replyErr)
+			wg.Done()
+		}
+		fmt.Fprint(conn, replyMessage)
 	}
-	fmt.Println("Message received: ", message)
-
-	fmt.Print("Send message: ")
-	replyReader := bufio.NewReader(os.Stdin)
-	replyMessage, replyErr := replyReader.ReadString('\n')
-	if replyErr != nil {
-		log.Fatal("Error: ", replyErr)
-	}
-	fmt.Fprint(conn, replyMessage)
 }
 
-// RunGuest takes destination ip
-// as argument and connects to that ip
+// RunGuest takes a destination IP as an
+// argument and connects to that IP
 func RunGuest(ip string) {
+	var wg sync.WaitGroup
+	wg.Add(2)
+
 	ipAndPort := ip + ":" + port
 	conn, dialErr := net.Dial("tcp", ipAndPort)
 	if dialErr != nil {
 		log.Fatal("Error: ", dialErr)
 	}
+	go handleGuestRead(conn, &wg)
+	go handleGuestSend(conn, &wg)
+
+	wg.Wait()
+
+}
+
+func handleGuestRead(conn net.Conn, wg *sync.WaitGroup) {
 	for {
-		handleGuest(conn)
+		replyReader := bufio.NewReader(conn)
+		replyMessage, replyErr := replyReader.ReadString('\n')
+		if replyErr != nil {
+			log.Fatal("Error: ", replyErr)
+			wg.Done()
+		}
+		fmt.Println("Message received:", replyMessage)
 	}
 }
 
-func handleGuest(conn net.Conn) {
-	fmt.Print("Send message: ")
-	reader := bufio.NewReader(os.Stdin)
-	message, readErr := reader.ReadString('\n')
-	if readErr != nil {
-		log.Fatal("Error: ", readErr)
+func handleGuestSend(conn net.Conn, wg *sync.WaitGroup) {
+	for {
+		reader := bufio.NewReader(os.Stdin)
+		message, readErr := reader.ReadString('\n')
+		if readErr != nil {
+			log.Fatal("Error: ", readErr)
+			wg.Done()
+		}
+		fmt.Fprint(conn, message)
 	}
-	fmt.Fprint(conn, message)
-
-	replyReader := bufio.NewReader(conn)
-	replyMessage, replyErr := replyReader.ReadString('\n')
-	if replyErr != nil {
-		log.Fatal("Error: ", replyErr)
-	}
-	fmt.Println("Message received:", replyMessage)
 }
